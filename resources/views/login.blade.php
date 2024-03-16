c128b18d35b464d77b85b6c29c5c5d6c63e69749
narges-h <narges.80.hoseyni@gmail.com>
2024-03-16 17:23:16 +0330

    page css

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -6,23 +6,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body style="text-align:center;background-color: linen;" >
+<body >
 <div>
-    <h2>Login</h2>
+    <h2 style="text-align:center">Login</h2>
 
-    <form>
+    <form style="text-align:center" action="/admin" method="get">
         @csrf
-        <div >
+        <div style="padding: 1% 4% 1% 0%;">
             <label>UserName</label>
-            <input type="text" name="username">
+            <input  type="text" name="username">
         </div>
-        <div>
+        <div style="padding: 1% 3.5% 1% 0%;">
             <label>password</label>
-            <input type="password" name="password">
+            <input  type="password" name="password">
         </div>
         
-        <div >
-            <input  type='submit' value="Login">
+        <div style="padding-left: 8%">
+            <input style="background-color: deepskyblue;padding: 1%;border: none;border-radius: 5%;color: white;font-family: system-ui;"
+              type='submit' value="Login">
         </div>
     </form>
    
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -9,7 +9,7 @@
 <body >
     <p style="text-align: center;color: cornflowerblue;font-family: system-ui;">وبلاگ های امروز</p>
 
-    <form  style="text-align:center" action="/admin" method="get">
+    <form  style="text-align:center" action="/login" method="get">
         <input style="background-color: deepskyblue;padding: 1%;border: none;border-radius: 5%;color: white;font-family: system-ui;"
          type='submit' value="ورود به بخش مدریت">    
     </form>
